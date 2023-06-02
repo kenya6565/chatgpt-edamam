bf1c9472bfa276c5fb2a15cc9759cb7775a0029b
kenya6565 <ken.australia0605@gmail.com>
2023-06-03 01:53:50 +0900

    create chat.ts

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,25 +1,31 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { ChatCompletionRequest, OpenAIAPI } from 'openai';
+import { OpenAIApi, Configuration } from 'openai';
 
-const openai = new OpenAIAPI({ key: process.env.OPENAI_SECRET_KEY });
+// OpenAI API の設定を作成
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
-// Edamam API setup here...
+// OpenAI API クライアントを初期化
+const openai = new OpenAIApi(configuration);
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   if (req.method === 'POST') {
-    const chatRequest = new ChatCompletionRequest({
-      model: 'text-davinci-002',
-      messages: req.body.messages,
+    const prompt = req.body.message;
+    const maxTokens = 60;
+
+    const gptResponse = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: prompt,
+      max_tokens: maxTokens,
     });
-    const chatResponse = await openai.completeChat(chatRequest);
-    const userMessage = chatResponse['choices'][0]['message']['content'];
 
     // Here you might call Edamam API with user's message, parse the result, and include it in your GPT-3's messages.
 
-    res.status(200).json({ chat: chatResponse });
+    res.status(200).json({ chat: gptResponse.data });
   } else {
     res.status(405).end(); // Method Not Allowed
   }
